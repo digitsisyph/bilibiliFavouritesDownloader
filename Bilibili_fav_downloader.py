20f3e539f7b941143ebfad14bbd61897e781c7ad
AbyssLink <26038696+AbyssLink@users.noreply.github.com>
2020-08-22 01:06:19 +0800

    Use RSSHub to replace unavailable API

--- Bilibili_fav_downloader.py
+++ Bilibili_fav_downloader.py
@@ -1,70 +1,65 @@
-import urllib.request
 import re
 import sys
+import urllib.request
 from subprocess import call
+
+import requests
+from bs4 import BeautifulSoup
+
+from config import Config
 
 
 class Bili_fav:
 
-    video_ids = []
+    video_urls = []
     video_titles = []
 
     # get url address 获取收藏夹地址
     def __init__(self, user_id, favorites_id):
-        self.favurl ="https://api.bilibili.com/x/space/fav/arc?vmid={}&fid={}".format(user_id, favorites_id)
+        self.favurl = f"https://rsshub.app/bilibili/fav/{user_id}/{favorites_id}"
 
     # load videos addresses 读取视频地址
     def load_favorites(self):
-        page = 1
-        while True:
-            response = urllib.request.urlopen(self.favurl+"&pn={}".format(page))
-            content = response.read().decode('utf-8')
-            id_results = re.findall('"aid":(\d+),', content)
-            title_results = re.findall('"highlight_title":"(.*?)"', content)
-
-            if not id_results: 
-                break
-            else:
-                page += 1
-            
-            # store titles and videos ids
-            self.video_titles += title_results
-            for video_id in id_results:
-                if video_id not in self.video_ids:
-                    self.video_ids.append(video_id)
+        r = requests.get(self.favurl)
+        print(r.status_code)
+        soup = BeautifulSoup(r.text, 'xml')
+        for item in soup.find_all('item'):
+            self.video_titles.append(item.title.text)
+            self.video_urls.append(item.link.text)
 
     # report spider results 显示爬虫结果
     def report(self):
-        print("已搜索到%s部视频：" % len(self.video_ids))
+        print("已搜索到%s部视频：" % len(self.video_urls))
         i = 1
         for title in self.video_titles:
-            print("%s."%i + title)
+            print("%s." % i + title)
             i += 1
-    
+
     # 下载视频
-    def download_videos(self, output_dir = ''):
+    def download_videos(self, output_dir=''):
         i = 1
         if output_dir == '':
-            for video_id in self.video_ids:
-                print("第%s/%s部视频下载中：（使用 ctrl+c 暂停下载）" % (i, len(self.video_ids)))
-                call("you-get --playlist https://www.bilibili.com/video/av{}".format(video_id), shell=True)
+            for video_url in self.video_urls:
+                print("第%s/%s部视频下载中：（使用 ctrl+c 暂停下载）" %
+                      (i, len(self.video_urls)))
+                call(
+                    f"you-get --playlist {video_url}", shell=True)
                 i += 1
-        else: 
-            for video_id in self.video_ids:
-                print("第%s/%s部视频下载中：（使用 ctrl+c 暂停下载）" % (i, len(self.video_ids)))
-                call("you-get --playlist -o {} https://www.bilibili.com/video/av{}".format(output_dir, video_id), shell=True)
+        else:
+            for video_url in self.video_urls:
+                print("第%s/%s部视频下载中：（使用 ctrl+c 暂停下载）" %
+                      (i, len(self.video_urls)))
+                call(
+                    f"you-get --playlist -o {output_dir} {video_url}", shell=True)
                 i += 1
 
 
 if __name__ == '__main__':
 
-    # 请输入用户id和收藏夹id
-    user_id = 111111
-    favorites_id = 11111
-    # 请输入下载目录
-    output_dir = "/Usr/...."
-
+    user_id = Config.USER_ID
+    favorites_id = Config.FAV_ID
+    output_dir = Config.OUTPUT_DIR
     my_fav = Bili_fav(user_id, favorites_id)
     my_fav.load_favorites()
     my_fav.report()
-    my_fav.download_videos(output_dir)  
+    my_fav.download_videos(output_dir)
---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    # 请输入用户id和收藏夹id
+    USER_ID = 2267573
+    FAV_ID = 488529573
+    # 请输入下载目录
+    OUTPUT_DIR = "."
